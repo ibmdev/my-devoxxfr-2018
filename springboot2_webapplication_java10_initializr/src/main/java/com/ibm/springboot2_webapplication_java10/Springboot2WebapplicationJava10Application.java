2a2a8bea7213a81f64ffc5b96b6dd06e4e3ae2bd
ibmdev <ibmdev102017@gmail.com>
2018-04-22 19:31:13 +0200

    Initial commit Spring Boot 2 - Spring Initializr - Java 10

---
+++ springboot2_webapplication_java10_initializr/src/main/java/com/ibm/springboot2_webapplication_java10/Springboot2WebapplicationJava10Application.java
@@ -0,0 +1,12 @@
+package com.ibm.springboot2_webapplication_java10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebapplicationJava10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2WebapplicationJava10Application.class, args);
+	}
+}
---
+++ springboot2_webapplication_java10_initializr/src/test/java/com/ibm/springboot2_webapplication_java10/Springboot2WebapplicationJava10ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.springboot2_webapplication_java10;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2WebapplicationJava10ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
